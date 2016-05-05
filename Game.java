8110ae1cf9f919aa41e7e8836aa5c7a50e11d38c
Manuel <manubp9@gmail.com>
2016-05-05 11:12:23 +0200

    123 cambiado lenguaje a italiano y utilizados switch

--- Game.java
+++ Game.java
@@ -3,7 +3,7 @@ import java.util.Stack;
  *  This class is the main class of the "World of Zuul" application. 
  *  "World of Zuul" is a very simple, text based adventure game.  Users 
  *  can walk around some scenery. That's all. It should really be extended 
- *  to make it more interesting!
+ *  to make it more interesting!~:-)
  * 
  *  To play this game, create an instance of this class and call the "play"
  *  method.
